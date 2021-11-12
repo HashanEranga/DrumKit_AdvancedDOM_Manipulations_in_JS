15e6b498c2cab7b866b36d2b9c36901847f08862
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-11-12 14:28:54 +0530

    add():animations to the buttons

--- index.js
+++ index.js
@@ -4,22 +4,39 @@ for(var i=0; i <  document.querySelectorAll('button').length; i++){
 
 function handleClick(){
     var innerHTMLContent = this.innerHTML;
+    addSound(innerHTMLContent)
+    buttonAnimation(innerHTMLContent)
+}
 
-    switch (innerHTMLContent) {
+document.addEventListener("keypress", function(event) {
+    addSound(event.key)
+    buttonAnimation(event.key)
+})
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey)
+    activeButton.classList.add('pressed')
+    setTimeout(function() {
+        activeButton.classList.remove('pressed')
+    },100)
+}
+
+function addSound(key){
+    switch (key){
         case "w":
-            var audio = new Audio("sounds/tom-1.mp3")
-            audio.play()
-        break;
-        case "a":
             var audio = new Audio("sounds/tom-2.mp3")
             audio.play()
         break;
-        case "s":
+        case "a":
             var audio = new Audio("sounds/tom-3.mp3")
             audio.play()
         break;
-        case "d":
+        case "s":
             var audio = new Audio("sounds/tom-4.mp3")
+            audio.play()
+        break;
+        case "d":
+            var audio = new Audio("sounds/tom-2.mp3")
             audio.play()
         break;
         case "j":
@@ -39,39 +56,3 @@ function handleClick(){
         break;
     }
 }
-
-document.addEventListener("keypress", function(event) {
-    switch (event.key){
-        case "w":
-            var audio = new Audio("sounds/tom-1.mp3")
-            audio.play()
-        break;
-        case "a":
-            var audio = new Audio("sounds/tom-2.mp3")
-            audio.play()
-        break;
-        case "s":
-            var audio = new Audio("sounds/tom-3.mp3")
-            audio.play()
-        break;
-        case "d":
-            var audio = new Audio("sounds/tom-4.mp3")
-            audio.play()
-        break;
-        case "j":
-            var audio = new Audio("sounds/snare.mp3")
-            audio.play()
-        break;
-        case "k":
-            var audio = new Audio("sounds/kick-bass.mp3")
-            audio.play()
-        break;
-        case "l":
-            var audio = new Audio("sounds/crash.mp3")
-            audio.play()
-        break;
-        default:
-            console.log("Invalid input")
-        break;
-    }
-})
